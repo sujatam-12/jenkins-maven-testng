dd9d1227afb88d92588be8cc9aa501f460e55ec3
Suju_M <sujatacu76@gmail.com>
2026-01-20 16:28:30 +0530

    Fixed Login tests - Jenkins stable

--- src/main/java/com/selenium/selenium_maven/base/BaseTest.java
+++ src/main/java/com/selenium/selenium_maven/base/BaseTest.java
@@ -1,4 +1,79 @@
+/*package com.selenium.selenium_maven.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    protected void launchBrowser(String browser) {
+
+        if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.pl/index.php");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}*/
+
+/*package com.selenium.selenium_maven.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        launchBrowser("chrome"); // ✅ DRIVER IS NOW INITIALIZED
+    }
+
+    protected void launchBrowser(String browser) {
+
+        if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.pl/index.php");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}*/
+
 package com.selenium.selenium_maven.base;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,7 +101,7 @@ public class BaseTest {
 
     protected void launchBrowser(String browser) {
 
-        if (browser.equalsIgnoreCase("firefox")) {
+        if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
         } else {
@@ -35,8 +110,7 @@ public class BaseTest {
         }
 
         driver.manage().window().maximize();
-        //driver.get("http://automationpractice.pl/index.php");
-        driver.get("https://automationexercise.com");
+        driver.get("http://automationpractice.pl/index.php");
     }
     /* ---------- CLEANUP ---------- */
     @AfterMethod(alwaysRun = true)
@@ -55,53 +129,4 @@ public class BaseTest {
     }
 }
 
-/*package com.selenium.selenium_maven.base;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.selenium.selenium_maven.utils.ExtentManager;
-
-public class BaseTest {
-
-    protected WebDriver driver;
-    protected static ExtentReports extent;
-
-    @Parameters("browser")
-    @BeforeMethod
-    public void setUp(@Optional("chrome") String browser) {
-
-        if (extent == null) {
-            extent = ExtentManager.getExtentReport();
-        }
-
-        if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
-
-        driver.manage().window().maximize();
-        driver.get("https://automationexercise.com");
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void flushReport() {
-        if (extent != null) {
-            extent.flush();
-        }
-    }
-}*/
 
--- src/main/java/com/selenium/selenium_maven/pages/PromoFramePage.java
+++ src/main/java/com/selenium/selenium_maven/pages/PromoFramePage.java
@@ -26,7 +26,7 @@ package com.selenium.selenium_maven.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.aventstack.extentreports.ExtentTest;
